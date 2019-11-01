a8b8f12e83c93d20f08e4857331a789ea56e2934
Donovan Baarda <abo@minkirri.apana.org.au>
2019-11-01 16:58:43 +1100

    Add lowpass error filter support and LowPassFilter2.
    This adds support to PIDController for lowpass filtering the error input to
    reduce steps and noise in the setpoint and/or feedback.
    
    It also adds LowPassFilter2 which supports different timeconstants for rising
    and falling values. This is useful for decaying peak/trough filters.

--- PIDController.py
+++ PIDController.py
@@ -15,8 +15,10 @@ class PIDController(object):
   * The output is range limited for predictable output.
   * The integrator is range limited and preloaded based on the output range
     to avoid integrator windup.
-  * There is a low-pass "RC" filter on the derivative to avoid derivative
-    noise spikes.
+  * There is a lowpass filter on the error input to reduce setpoint steps
+    and/or feedback noise.
+  * There is a lowpass filter on the derivative to avoid derivative noise
+    spikes.
   * The derivative low-pass filter means this works for step inputs with
     dt=0.0.
 
@@ -27,7 +29,8 @@ class PIDController(object):
     Kp: The PID proportional gain.
     Ki: The PID integral gain.
     Kd: The PID derivative gain.
-    Ld: The derivative low-pass filter timeconstant.
+    Ld: The derivative lowpass filter timeconstant.
+    Le: The error lowpass filter timeconstant.
     error: The last Kp scaled error.
     integ: The last Ki scaled integral.
     deriv: The last Kd scaled derivative.
@@ -39,40 +42,44 @@ class PIDController(object):
   integMin = outputMin - 1.0 * (outputMax - outputMin)
   integMax = outputMax + 1.0 * (outputMax - outputMin)
 
-  def __init__(self, Kp, Ki, Kd, Ld=0.0):
+  def __init__(self, Kp, Ki, Kd, Ld=0.0, Le=0.0):
     self.Kp = Kp
     self.Ki = Ki
     self.Kd = Kd
     self.Ld = Ld
+    self.Le = Le
     self.error = 0.0
     self.integ = (self.outputMin + self.outputMax) / 2.0
     self.deriv = 0.0
     self.output = self.integ
 
   @classmethod
-  def StandardForm(cls, Kp, Ti, Td, Ld=None):
+  def StandardForm(cls, Kp, Ti, Td, Ld=None, Le=None):
     """Standard form PIDController initializer.
 
     This initialization uses the times over which the T and D terms
     effectively operate. Ti is how long in the past I term looks, and
     Td is how far in the future the D term looks. If Ld is None it
-    will default to Td/8.
+    will default to Td/8. If Le is None it defaults to Ld/8.
 
     Args:
       Kp: The PID proportional gain.
       Ti: The PID integral time.
       Td: The PID derivative time.
-      Ld: The derivative low-pass filter RC time (default: Td/8)
+      Ld: The derivative lowpass filter timeconstant (default: Td/8)
+      Le: The error lowpass filter timeconstant (default: Ld/8)
 
     Returns:
       An initialized PIDController.
     """
     if Ld is None:
-        Ld = Td / 8.0
-    return cls(Kp, 1.0/Ti, Td, Ld)
+      Ld = Td / 8.0
+    if Le is None:
+      Le = Ld / 8.0
+    return cls(Kp, 1.0/Ti, Td, Ld, Le)
 
   @classmethod
-  def ZiglerNichols(cls, Ku, Tu, Ld=None):
+  def ZiglerNichols(cls, Ku, Tu, Ld=None, Le=None):
     """ZiglerNichols PIDController initializer.
 
     This initialization uses the Classic PID Zeigler-Nichols tuning
@@ -88,7 +95,7 @@ class PIDController(object):
     Returns:
       An initialized PIDController.
     """
-    return cls.StandardForm(0.6*Ku, Tu/2.0, Tu/8.0, Ld)
+    return cls.StandardForm(0.6*Ku, Tu/2.0, Tu/8.0, Ld, Le)
 
   def update(self, error, dt=1.0):
     """Update with a new input.
@@ -102,8 +109,10 @@ class PIDController(object):
     Returns:
       The control output.
     """
-    # Calculate proportional term.
+    # Calculate proportional term and lowpass filter if needed.
     error = self.Kp * error
+    if self.Le:
+      error = (dt * error + self.Le * self.error) / (dt + self.Le)
     # Accumulate trapezoidal integral of error scaled by Ki.
     integ = self.Ki * dt * (error + self.error) / 2.0 + self.integ
     # Limit integral term between integMin and integMax.
@@ -140,8 +149,8 @@ class PIDController(object):
     return self.update(error, dt)
 
   def __repr__(self):
-    return "PIDController(Kp=%5.3f, Ki=%5.3f, Kd=%5.3f, Ld=%5.3f)" % (
-        self.Kp, self.Ki, self.Kd, self.Ld)
+    return "PIDController(Kp=%5.3f, Ki=%5.3f, Kd=%5.3f, Ld=%5.3f, Le=%5.3f)" % (
+        self.Kp, self.Ki, self.Kd, self.Ld, self.Le)
 
   def __str__(self):
     return "%r: error=%+6.3f integ=%+6.3f deriv=%+6.3f output=%+6.3f" % (
@@ -159,7 +168,30 @@ class LowPassFilter(object):
     return self.output
 
   def __repr__(self):
-    return "LowPass(T=%5.3f)" % self.T
+    return "LowPassFilter(T=%5.3f)" % self.T
+
+  def __str__(self):
+    return "%r: output=%+6.3f" % (self, self.output)
+
+
+class LowPassFilter2(object):
+  "A lowpass filter with different timeconstants for rising and falling."""
+
+  def __init__(self, Tup, Tdn, output=0.0):
+    self.Tup = Tup
+    self.Tdn = Tdn
+    self.output = output
+
+  def update(self, value, dt=1.0):
+    if value > self.output:
+      T = self.Tup
+    else:
+      T = self.Tdn
+    self.output = (value*dt + self.output*T) / (T + dt)
+    return self.output
+
+  def __repr__(self):
+    return "LowPassFilter2(Tup=%5.3f, Tdn=%5.3f)" % (self.Tup, self.Tdn)
 
   def __str__(self):
     return "%r: output=%+6.3f" % (self, self.output)
@@ -168,6 +200,7 @@ class LowPassFilter(object):
 if __name__ == '__main__':
   controller = PIDController.ZiglerNichols(Ku=5.0, Tu=15.0)
   demand = 1.0
+  force = 0.0
   mass = 10.0
   position = 0.0
   velocity = 0.0
@@ -175,7 +208,7 @@ if __name__ == '__main__':
   controller.reset(demand - position)
   print "%2d %6.3f %s" % (0, position, controller)
   for t in xrange(40):
-    force = controller.update(demand - position, 1.0)
     velocity += force/mass
     position += velocity
+    force = controller.update(demand - position, 1.0)
     print "%2d %6.3f %s" % (t, position, controller)
